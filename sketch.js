451dfd7a795481fd3a51adbd04c28185369b3e15
Pengiii18 <ab198035@gmail.com>
2025-05-29 19:33:08 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,24 @@
 let capture;
+let handposeModel;
+let predictions = [];
 
 function setup() {
   createCanvas(400, 400);
   capture = createCapture(VIDEO);
   capture.size(400, 400);
   capture.hide();
+
+  handposeModel = ml5.handpose(capture, modelReady);
+  handposeModel.on("predict", gotResults);
+}
+
+function modelReady() {
+  console.log("Handpose model loaded!");
+}
+
+function gotResults(results) {
+  predictions = results;
+  console.log(predictions);
 }
 
 function draw() {
